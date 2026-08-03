3fdb706cb46be6f6579ae64f366177ee01e66949
Siddhant Singh <sidshekhawat16@gmail.com>
2026-08-03 21:33:53 +0530

    feat: complete arithmetic operators

---
+++ 01-basics/05-arithmetic.cpp
@@ -0,0 +1,32 @@
+/*
+arithmetic operators = return the result of a specific arithmetic operation (+ - * /)
+*/
+//parenthesis
+//multiplication & division
+//addition & subtraction
+ 
+#include <iostream>
+
+int main() {
+
+    //double students = 20;
+
+    int students = 6 - (5 + 4) * 3 / 2;
+
+    //students = students + 1;
+    //students +=2;
+    //students++; //only to add 1
+
+    //students = students - 1;
+    //students -=1;
+    //students--;
+
+    //students = students * 2;
+    //students*= 2;
+
+    //students = students / 2;
+    //students/= 3;   
+
+    std::cout << "Number of Students: " << students << std::endl;
+
+}
