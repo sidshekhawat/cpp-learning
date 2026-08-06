563706f3a2628ca088fd8bf705c4d1be7d35333b
Siddhant Singh <sidshekhawat16@gmail.com>
2026-08-06 11:22:31 +0530

    feat: complete for loop

---
+++ 02-control-flow/07-for_loop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+    for(int i = 10; i >= 0; i--){
+        std::cout << i <<std::endl;
+    }
+    std::cout << "Happy New Year!" <<std::endl;
+
+    return 0;
+}
