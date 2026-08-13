784fc25471dfca180040b62334a06b6e0b4e3cf4
Siddhant Singh <sidshekhawat16@gmail.com>
2026-08-13 09:12:47 +0530

    feat: complete pass by value vs reference

---
+++ 03-functions/05-pass_by_value_reference.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+void swap(std::string &x, std::string &y);
+
+int main(){
+
+    std:: string x = "Kool-Aid";
+    std:: string y = "Water";
+    
+    swap(x,y); 
+
+    std::cout << "X: " << x <<std::endl;
+    std::cout << "Y: " << y <<std::endl;
+
+
+    return 0;
+}
+void swap(std::string &x, std::string & y){
+
+    std:: string temp;
+    temp = x;
+    x = y;
+    y = temp;
+}
