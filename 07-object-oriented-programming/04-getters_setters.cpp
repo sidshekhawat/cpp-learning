170170993d6971aa6f18eccf58ce593247f261f6
Siddhant Singh <sidshekhawat16@gmail.com>
2026-08-16 18:01:01 +0530

    feat: complete getters and setters

---
+++ 07-object-oriented-programming/04-getters_setters.cpp
@@ -0,0 +1,39 @@
+/*
+Abstraction = hiding unnecessary data from outside a class
+getter = function that makes a private attribute READABLE
+setter = function that makes a private attribute WRITEABLE
+*/
+
+#include <iostream>
+
+class Stove{
+    private:
+        int temperature = 0;
+
+    public:
+    int getTemperature(){
+        return temperature;
+    }
+    void setTemperature(int temperature){
+        if(temperature < 0){
+            this-> temperature = 0; 
+        }
+        else if(temperature >= 10){
+            this-> temperature = 10;
+        }
+        else{
+            this-> temperature = temperature;
+        }
+    }
+}; 
+
+int main(){
+
+    Stove stove;
+
+    stove.setTemperature(100000);
+
+    std::cout << "The temperature setting is:  " <<stove.getTemperature() <<std::endl;
+
+    return 0;
+}
